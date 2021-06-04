2a47403d918b1819398c68fc83a1ed77927a6572
hermy991 <hermygarcia@gmail.com>
2021-06-04 17:03:00 -0400

    +information

--- src/decorators/columns/column.ts
+++ src/decorators/columns/column.ts
@@ -1,10 +1,18 @@
 import {ColumnOptions} from "../options/column_options.ts"
 import {getMetadata} from "../metadata/metadata.ts"
+import { Reflect } from "https://deno.land/x/reflect_metadata@v0.1.12/mod.ts";
+
+const ColumnMetadataKey = Symbol("Column");
+
+// http://tutorialspots.com/typescript-tutorial-lesson-29-metadata-reflection-5988.html
 
 export function Column(options: ColumnOptions = {}): any {
   return function (entity: Function, propertyKey: string, descriptor: PropertyDescriptor) {
     let mixeds: ColumnOptions = { name: propertyKey};
     mixeds = Object.assign(mixeds, options);
+
+    console.log(Reflect.metadata(ColumnMetadataKey, entity));
+
     getMetadata().columns.push({
       // path,
       entity,
