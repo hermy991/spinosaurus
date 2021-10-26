b8dacb4441731de0729a68148409bcd5b92e2986
hermy991 <hermygarcia@gmail.com>
2021-10-26 07:46:54 -0400

    feat(std,xml): upgrade to deno std@v0.113, xml@2.0.0

--- deps.ts
+++ deps.ts
@@ -6,11 +6,11 @@ declare global {
 }
 
 export * as postgres from "https://deno.land/x/postgres@v0.13.0/mod.ts";
-export * as path from "https://deno.land/std@0.111.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.111.0/fs/mod.ts";
-export * as yaml from "https://deno.land/std@0.111.0/encoding/yaml.ts";
-export * as hash from "https://deno.land/std@0.111.0/hash/mod.ts";
+export * as path from "https://deno.land/std@0.113.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.113.0/fs/mod.ts";
+export * as yaml from "https://deno.land/std@0.113.0/encoding/yaml.ts";
+export * as hash from "https://deno.land/std@0.113.0/hash/mod.ts";
 export * as reflect from "https://cdn.skypack.dev/@abraham/reflection";
 //export { Reflect as reflect } from "https://deno.land/x/reflect_metadata@v0.1.12-2/mod.ts";
 export * as dotenv from "https://deno.land/x/dotenv_parser@v1.0.2/mod.ts";
-export * as xml from "https://deno.land/x/xml@v1.0.3/mod.ts";
+export * as xml from "https://deno.land/x/xml@2.0.0/mod.ts";
