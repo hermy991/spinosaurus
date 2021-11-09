a28f9ee4e4c4c800418f1e83ba2ee46e0b74ac12
hermy991 <hermygarcia@gmail.com>
2021-11-09 17:51:04 -0400

    fix(dependencies): update dependencies

--- deps.ts
+++ deps.ts
@@ -5,11 +5,11 @@ declare global {
   }
 }
 
-export * as postgres from "https://deno.land/x/postgres@v0.13.0/mod.ts";
-export * as path from "https://deno.land/std@0.113.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.113.0/fs/mod.ts";
-export * as yaml from "https://deno.land/std@0.113.0/encoding/yaml.ts";
-export * as hash from "https://deno.land/std@0.113.0/hash/mod.ts";
+export * as postgres from "https://deno.land/x/postgres@v0.14.2/mod.ts";
+export * as path from "https://deno.land/std@0.114.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.114.0/fs/mod.ts";
+export * as yaml from "https://deno.land/std@0.114.0/encoding/yaml.ts";
+export * as hash from "https://deno.land/std@0.114.0/hash/mod.ts";
 export * as reflect from "https://cdn.skypack.dev/@abraham/reflection";
 //export { Reflect as reflect } from "https://deno.land/x/reflect_metadata@v0.1.12-2/mod.ts";
 export * as dotenv from "https://deno.land/x/dotenv_parser@v1.0.2/mod.ts";
